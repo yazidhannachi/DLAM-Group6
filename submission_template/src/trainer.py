77fdd5bcf1846ddb7e0005707d77ad1351262f6f
yvette <yvette-werner@web.de>
2026-09-03 00:19:11 +0200

    add gradient accumulation and weighted loss

--- submission_template/src/trainer.py
+++ submission_template/src/trainer.py
@@ -6,11 +6,13 @@ from datetime import datetime
 from tqdm import tqdm
 
 class Trainer:
-    def __init__(self, model, optimizer, criterion, scheduler=None):
+    def __init__(self, model, optimizer, criterion, scheduler=None, base_model=None, as_feature=None):
         self.model = model
         self.optimizer = optimizer
         self.criterion = criterion
         self.scheduler = scheduler
+        self.as_feature = as_feature # weather the output from the hybrid model should serve as additional feature or for training on residuals
+        self.base_model = base_model
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
@@ -18,7 +20,7 @@ class Trainer:
         self.loss_tracker = []
         self.vloss_tracker = []
 
-    def fit(self, save_path, train_loader, df_train, df_val, loss_on='target', mode='single_forecast', ar_steps=None, min_epochs=30, max_epochs=100, patience=10):
+    def fit(self, save_path, train_loader, df_train, df_val, loss_on='target', loss_weighted=False, alpha=None, mode='single_forecast', ar_steps=None, min_epochs=30, max_epochs=100, patience=10, grad_acc=False, grad_acc_steps=None):
         # mode single_forecast means we only use the direct output of xLSTM-Mixer (exactly pred_len steps) to compute training loss
         # mode "autoregressive" means we use xLSTM-Mixer predict pred_len steps, then autoregressively feed predictions back into model
         # to obtain next pred_len steps (just like in validation), repeating ar_steps times
@@ -35,12 +37,14 @@ class Trainer:
             self.model.train()
             running_loss = 0.0
             progress_bar = tqdm(train_loader, desc=f"Epoch {epoch+1}/{max_epochs}", leave=True)
+            if grad_acc:
+                self.optimizer.zero_grad(set_to_none=True)
             for i, batch in enumerate(train_loader):
                 x_enc, y, series_idx, _ = batch
                 x_enc = x_enc.to(self.device)
                 y = y.to(self.device)
-
-                self.optimizer.zero_grad(set_to_none=True)
+                if grad_acc == False:
+                    self.optimizer.zero_grad(set_to_none=True)
                 if mode=='single_forecast':
                     predictions = self.model.predict(x_enc, series_idx)
                 elif mode=='autoregressive':
@@ -49,11 +53,26 @@ class Trainer:
                 if loss_on=='all':
                     loss = self.criterion(predictions[:,:,:y.shape[-1]], y)
                 elif loss_on=='target':
-                    loss = self.criterion(predictions[:,:,target_idx], y[:,:,target_idx])
-                loss.backward()
-                self.optimizer.step()
-                
+                    if loss_weighted==True:
+                        self.criterion.reduction = 'none'
+                        loss = self.criterion(predictions[:,:,target_idx], y[:,:,target_idx])
+                        weights = 1.0 + alpha * torch.abs(y[:,:,target_idx])
+                        loss = (weights * loss).sum() / weights.sum()
+                    else:
+                        loss = self.criterion(predictions[:,:,target_idx], y[:,:,target_idx])
+
                 running_loss += loss.item()
+
+                if grad_acc == True:
+                    loss = loss/grad_acc_steps
+                    loss.backward()
+                    if ((i + 1) % grad_acc_steps == 0) or (i + 1 == len(train_loader)):
+                        self.optimizer.step()
+                        self.optimizer.zero_grad(set_to_none=True)
+                else:
+                    loss.backward()
+                    self.optimizer.step()
+                
                 if i % 50 == 0 or i==(len(train_loader)-1):
                     step = i % 50 if (i == len(train_loader) and i % 50 != 0) else 50
                     progress_bar.set_postfix({"loss": f"{running_loss / (i+1):.4f}"})
@@ -67,19 +86,25 @@ class Trainer:
             self.model.eval()
             #val_bar = tqdm(val_loader, desc=f"Validation", leave=False)
             with torch.no_grad():
-                '''for vdata in val_loader:
-                    vx, vy, series_idx = vdata
-                    vx = vx.to(self.device)
-                    vy = vy.to(self.device)
-                    vpred= self.model.predict(vx, series_idx=series_idx)
-                    vloss = self.criterion(vpred[:,:,:vy.shape[-1]], vy)
-                    running_vloss += vloss.item()
-                    val_bar.update(1)
-                    val_bar.set_postfix(batch_loss=f"{vloss.item():.4f}")'''
-                pred_df = self.model.predict_autoregressive(df_train, df_val)
-                assert pred_df.index.equals(df_val.index)
-                pred = torch.tensor(pred_df["target"].values, dtype=torch.float32, device=self.device)
-                true = torch.tensor(df_val["target"].values, dtype=torch.float32, device=self.device)
+                if self.base_model is not None:
+                    pred_df = self.model.predict_autoregressive_hybrid(
+                        df_train,
+                        df_val,
+                        base_model=self.base_model,
+                        as_feature=self.as_feature,
+                    )
+                else:
+                    pred_df = self.model.predict_autoregressive(df_train, df_val)
+                
+                pred_eval = pred_df.groupby("series_id").tail(336)
+                true_eval = df_val.groupby("series_id").tail(336)
+
+                assert pred_eval.index.equals(true_eval.index)
+
+                pred = torch.tensor(pred_eval["target"].values, dtype=torch.float32, device=self.device)
+                true = torch.tensor(true_eval["target"].values, dtype=torch.float32, device=self.device)
+
+                self.criterion.reduction='mean'
                 vloss = self.criterion(pred, true).item()
 
             self.vloss_tracker.append(vloss)
